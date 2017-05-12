edf537702f4ce83dbe6becb6c36c39cb7473bc57
e1500844 <e1500844@edu.vamk.fi>
2017-05-12 23:23:53 +0300

    Käyttöliittymä, TagController yms

--- src/main/java/project/controller/TagController.java
+++ src/main/java/project/controller/TagController.java
@@ -1,5 +1,46 @@
 package project.controller;
 
-public class TagController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import project.model.Tag;
+import project.repository.TagRepository;
+
+@RestController
+public class TagController {
+	@Autowired
+	TagRepository repository;
+	@RequestMapping(value = "/tag", method = RequestMethod.GET)
+	public Tag get(@RequestParam(value = "id", defaultValue = "1") int id) {
+		return repository.findOne(id);
+	}
+	
+	
+	@RequestMapping(value = "/tag", method = RequestMethod.POST)
+	public @ResponseBody Tag addTag(@RequestBody Tag jsonTag) {
+		repository.save(jsonTag);
+		return jsonTag;
+	}
+
+	@RequestMapping(value = "/tag", method = RequestMethod.DELETE)
+	public void deleteTag(@RequestBody Tag jsonTag) {
+		repository.delete(jsonTag);
+		// do business logic
+	}
+
+	@RequestMapping(value = "/tag", method = RequestMethod.PUT)
+	public @ResponseBody Tag updateTag(@RequestBody Tag jsonTag) {
+		repository.save(jsonTag);
+		return jsonTag;
+	}
+	
+	@RequestMapping(value = "/tag/all", method = RequestMethod.GET)
+	public Iterable<Tag> get() {
+		return repository.findAll();
+	}
 }
